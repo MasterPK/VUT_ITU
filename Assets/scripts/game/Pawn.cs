00a8c0b9a965a2aa12fa802dace31699a06550f5
Martin Klobušický <martin.klobusicky@centrum.sl>
2019-12-03 14:56:42 +0100

    push

--- Assets/scripts/game/Pawn.cs
+++ Assets/scripts/game/Pawn.cs
@@ -3,5 +3,35 @@ using UnityEngine.UI;
 
 public class Pawn : BasePiece
 {
+    public override void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
+    {
+        base.Setup(newTeamColor, newSpriteColor, newPieceManager);
 
+        mMovement = mColor == Color.white ? new Vector3Int(0, 1, 1) : new Vector3Int(0, -1, -1);
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Pawn");
+ 
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+
+    }
+    private bool MatchesState(int targetX, int targetY, CellState targetState)
+    {
+        CellState cellState = CellState.None;
+        cellState = mCurrentCell.mBoard.ValidateCell(targetX, targetY, this);
+
+        if(cellState == targetState)
+        {
+            mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+            return true;
+        }
+        return false;
+    }
+
+    protected override void CheckPathing()
+    {
+        
+    }
 }
