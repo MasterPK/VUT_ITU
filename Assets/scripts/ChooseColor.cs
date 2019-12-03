dd8aacdac4a4df0361642348304752474cc850f5
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-12-03 14:57:33 +0100

    a

---
+++ Assets/scripts/ChooseColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseColor : MonoBehaviour
+{
+    public Image whiteImg;
+   public void SetImage(Image image)
+    {
+        whiteImg.sprite = Resources.Load<Sprite>("black");
+
+    }
+
+  
+}
